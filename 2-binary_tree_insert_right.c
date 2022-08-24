76107b38ee3fb074ec090f5e7db1a91952fd6fda
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-08-24 13:57:45 +0000

    fix right

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -13,7 +13,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 
 	if (!parent)
 		return (NULL);
-	prev_right = parent->left;
+	prev_right = parent->right;
 
 	node = malloc(sizeof(binary_tree_t));
 	if (!node)
