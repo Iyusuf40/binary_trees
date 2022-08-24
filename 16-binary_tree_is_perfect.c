c1768b2e8c9d0c687dda8f986b431481a93ad401
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-08-24 22:40:14 +0000

    because alx won't let me use logic

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -2,23 +2,25 @@
 #include <stdio.h>
 
 /**
- * is_full - checks if tree is full
- *
- * @tree: tree to check
- * Return: result sum
+ * binary_tree_leaves - computes the number of leaves in a tree
+ * @tree: tree to compute
+ * Return: size
  */
-int is_full(const binary_tree_t *tree)
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	int res = 0;
+	size_t size = 0;
 
 	if (!tree)
 		return (0);
-	if ((tree->left && !tree->right) || (!tree->left && tree->right))
+
+	if (!tree->left && !tree->right)
 		return (1);
 
-	res += is_full(tree->left);
-	res += is_full(tree->right);
-	return (res);
+	size += binary_tree_leaves(tree->left);
+	size += binary_tree_leaves(tree->right);
+
+	return (size);
 }
 
 /**
@@ -41,36 +43,24 @@ int height(const binary_tree_t *tree)
 }
 
 /**
- * boolian - checks if a tree is perfect
- * @tree: node to compute balance factor of
- * Return: 0 or 1
- */
-int boolian(const binary_tree_t *tree)
-{
-	int res = 0;
-
-	if (!tree)
-		return (0);
-	res += is_full(tree);
-	if (res)
-		return (res);
-	res = height(tree->left) - height(tree->right);
-	return (res);
-}
-
-/**
  * binary_tree_is_perfect - checks if a tree is perfect
  * @tree: node to compute balance factor of
  * Return: 0 or 1
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int res = 0;
+	int h = 0, res;
+	size_t leaves;
 
 	if (!tree)
 		return (0);
-	res = boolian(tree);
-	if (res)
-		return (0);
-	return (1);
+	h = height(tree) - 1;
+	leaves = binary_tree_leaves(tree);
+
+	res = 2 << (h - 1);
+
+	/*printf("res %d leaves %lu height %d\n", res, leaves, h);*/
+	if ((size_t)res == leaves)
+		return (1);
+	return (0);
 }
