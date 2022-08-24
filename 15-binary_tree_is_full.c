f182a7c5e004e282cb54ef5ca03be407abab29b0
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-08-24 20:12:04 +0000

    add fix

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -10,11 +10,13 @@ int is_full(const binary_tree_t *tree)
 {
 	int res = 0;
 
-	if (!tree->left || !tree->right)
-		res += 1;
+	if (!tree)
+		return (0);
+	if ((tree->left && !tree->right) || (!tree->left && tree->right))
+		return (1);
 
-	res += binary_tree_is_full(tree->left);
-	res += binary_tree_is_full(tree->right);
+	res += is_full(tree->left);
+	res += is_full(tree->right);
 	return (res);
 }
 
